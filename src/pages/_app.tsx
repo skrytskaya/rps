ec1e826e413a2f7c24fada0d243f8a04b0d8d46d
skrytskaya <viktoryia.skrytskaya@gmail.com>
2023-05-24 17:38:07 +0200

    feat: basic RPS game with a countdown and results

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Rock Paper Scissors</title>
+        <meta name="description" content="🪨📄✂️" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
