e7a93acfb0b027e0c8067a2ff56472b552d9d3de
skrytskaya <viktoryia.skrytskaya@gmail.com>
2023-05-25 10:08:04 +0200

    fix: correct the issue with updating localStorage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,47 +26,73 @@ const options: Option[] = [
   { id: 2, name: "scissors", icon: "✂️" },
 ];
 
-// TODO vik: fix issue with loosing score and count of the last game
-
+const isBrowser = typeof window !== "undefined";
 const Home: NextPage = () => {
   const [playerName, setPlayerName] = useState<Player["name"]>("");
-  const [players, setPlayers] = useState<Player[]>([]);
+  const [players, setPlayers] = useState<Player[]>(() => {
+    if (isBrowser) {
+      const savedPlayers = JSON.parse(
+        localStorage.getItem("players") ?? "[]"
+      ) as Player[];
+      return savedPlayers ? savedPlayers : [];
+    } else {
+      return [];
+    }
+  });
   const [computerOption, setComputerOption] = useState<Option | null>(null);
   const [playerOption, setPlayerOption] = useState<Option | null>(null);
   const [winner, setWinner] = useState<WinnerOptions | null>(null);
   const [isGameOn, setIsGameOn] = useState(false);
   const [countdown, setCountdown] = useState(3);
 
-  useEffect(() => {
-    const savedPlayers = JSON.parse(
-      localStorage.getItem("players") ?? "[]"
-    ) as Player[];
-    if (savedPlayers) {
-      setPlayers(savedPlayers);
-    }
+  const updatePlayerScore = useCallback((name: string, result: string) => {
+    setPlayers((prevPlayers) =>
+      prevPlayers.map((player) => {
+        if (player.name === name) {
+          return {
+            ...player,
+            score: {
+              ...player.score,
+              [result as keyof typeof player.score]:
+                player.score[result as keyof typeof player.score] + 1,
+            },
+            totalGames: player.totalGames + 1,
+          };
+        }
+        return player;
+      })
+    );
+    setIsGameOn(false);
   }, []);
 
-  const updatePlayerScore = useCallback(
-    (name: string, result: string) => {
-      setPlayers((prevPlayers) =>
-        prevPlayers.map((player) => {
-          if (player.name === name) {
-            return {
-              ...player,
-              score: {
-                ...player.score,
-                [result as keyof typeof player.score]:
-                  player.score[result as keyof typeof player.score] + 1,
-              },
-              totalGames: player.totalGames + 1,
-            };
-          }
-          return player;
-        })
-      );
-      localStorage.setItem("players", JSON.stringify(players));
+  useEffect(() => {
+    localStorage.setItem("players", JSON.stringify(players));
+  }, [players]);
+
+  const determineWinner = useCallback(
+    (playerOption: Option | null) => {
+      const randomIndex = Math.floor(Math.random() * options.length);
+      const selectedOption = options[randomIndex] ?? null;
+      setComputerOption(selectedOption);
+      if (playerOption?.id === selectedOption?.id) {
+        setWinner("Draw");
+        updatePlayerScore(playerName, "draws");
+      } else if (
+        (playerOption?.name === "rock" &&
+          selectedOption?.name === "scissors") ||
+        (playerOption?.name === "scissors" &&
+          selectedOption?.name === "paper") ||
+        (playerOption?.name === "paper" && selectedOption?.name === "rock")
+      ) {
+        setWinner("Player");
+        updatePlayerScore(playerName, "wins");
+      } else {
+        setWinner("Computer");
+        updatePlayerScore(playerName, "losses");
+      }
+      setIsGameOn(false);
     },
-    [players]
+    [playerName, updatePlayerScore]
   );
 
   useEffect(() => {
@@ -78,45 +104,40 @@ const Home: NextPage = () => {
       }, 1000);
     } else if (countdown === 0 && isGameOn) {
       setCountdown(0);
-      const randomIndex = Math.floor(Math.random() * options.length);
-      const selectedOption = options[randomIndex] ?? null;
-      setComputerOption(selectedOption);
-      setWinner("Computer");
-      setPlayerOption({ icon: "🤷", name: "none", id: options.length + 1 });
-      updatePlayerScore(playerName, "losses");
+      const skippedOption = {
+        id: options.length + 1,
+        name: "none",
+        icon: "🤷",
+      };
+      setPlayerOption(skippedOption);
+      determineWinner(skippedOption);
+
       setIsGameOn(false);
     }
-
     return () => clearTimeout(countdownTimer);
-  }, [countdown, isGameOn, playerName, updatePlayerScore]);
+  }, [
+    countdown,
+    determineWinner,
+    isGameOn,
+    playerName,
+    playerOption,
+    updatePlayerScore,
+    winner,
+  ]);
+
+  useEffect(() => {
+    const savedPlayers = JSON.parse(
+      localStorage.getItem("players") ?? "[]"
+    ) as Player[];
+    if (savedPlayers) {
+      setPlayers(savedPlayers);
+    }
+  }, []);
 
   const handlePlayerOption = (optionId: number) => {
-    setPlayerOption(options[optionId] ?? null);
-    const randomIndex = Math.floor(Math.random() * options.length);
-    const selectedOption = options[randomIndex] ?? null;
-    setComputerOption(selectedOption ?? null);
-    determineWinner(options[optionId] ?? null, selectedOption);
-  };
-
-  const determineWinner = (
-    playerOption: Option | null,
-    computerOption: Option | null
-  ) => {
-    if (playerOption?.id === computerOption?.id) {
-      setWinner("Draw");
-      updatePlayerScore(playerName, "draws");
-    } else if (
-      (playerOption?.name === "rock" && computerOption?.name === "scissors") ||
-      (playerOption?.name === "scissors" && computerOption?.name === "paper") ||
-      (playerOption?.name === "paper" && computerOption?.name === "rock")
-    ) {
-      setWinner("Player");
-      updatePlayerScore(playerName, "wins");
-    } else {
-      setWinner("Computer");
-      updatePlayerScore(playerName, "losses");
-    }
     setIsGameOn(false);
+    setPlayerOption(options[optionId] ?? null);
+    determineWinner(options[optionId] ?? null);
   };
 
   const handleStartGame = () => {
